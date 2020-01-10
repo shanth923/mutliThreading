ad03c89f216dd35382e2884cc058af20c1146a21
shanth kuamer <shanth923@gmail.com>
2020-01-10 14:24:24 +0530

    threads done

--- mutliThreading/ViewController.swift
+++ mutliThreading/ViewController.swift
@@ -12,9 +12,67 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+      multiThread()
+        
+        
         // Do any additional setup after loading the view.
     }
 
+    
 
-}
+    func multiThread(){
+        
+        
+        var thread1 = DispatchQueue(label: "thread1")
+        var thread2 = DispatchQueue(label: "thread2")
+        var thread3 = DispatchQueue(label: "thread3")
+        
 
+        
+//  sync when thrad run no thread where not runs a parallelly
+        
+        
+        thread1.sync {
+            
+            for i in 0...100{
+                
+                print("@@@@@@@@ \(i)")
+               
+                
+                
+            }
+        }
+       thread2.async {
+            
+            for i in 0...100{
+                
+                print("######## \(i)")
+                
+            }
+        }
+        
+        thread3.async {
+        
+        for i in 0...100{
+            
+            print("$$$$$$$$ \(i)")
+            
+        }
+        
+        }
+            
+            
+            
+        }
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
